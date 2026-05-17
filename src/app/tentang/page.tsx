020e0552c0dbb880b92b45c17f45463ef57620e1
tumbasofficial <tumbasofficial1@gmail.com>
2026-05-17 12:45:54 +0700

    Add files via upload

--- src/app/tentang/page.tsx
+++ src/app/tentang/page.tsx
@@ -1,7 +1,7 @@
 export default function HalamanTentang() {
   return (
     <div style={{ padding: '40px 20px', fontFamily: 'sans-serif', maxWidth: '800px', margin: '0 auto', textAlign: 'center' }}>
-      <h1>Tentang Tarumaya Leather</h1>
+      <h1>Tentang Tumbas Official</h1>
       <p style={{ lineHeight: '1.8', color: '#555', marginTop: '20px' }}>
         "TUMBAS OFFICIAL merupakan platform e-commerce terintegrasi yang dirancang untuk menyediakan ekosistem perdagangan digital yang inklusif, aman, dan efisien. Mengusung misi untuk menjembatani pelaku usaha dengan pasar yang lebih luas, platform ini mengintegrasikan manajemen katalog produk yang dinamis, sistem pelacakan transaksi yang real-time, serta pengelolaan keranjang belanja yang responsif guna menghadirkan pengalaman belanja yang fleksibel bagi pengguna.
 
